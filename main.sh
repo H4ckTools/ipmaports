16e20e846b8915d49d377958635b15fdc7f0b09a
k0k41 <k0k41@localhost.localdomain>
2022-01-27 00:08:00 +0100

    cambios realizados, comandos nmpa y ping

--- main.sh
+++ main.sh
@@ -15,6 +15,12 @@ main () {
     error "$0: error: usage: $0 <ip>"
   fi
   echo $ip
+  echo "Enviando paquetes a la ip"
+    ping -c 1 $ip
+  echo "Escaneando puertos de la ip con "
+    nmap -p- --open -Tx -v -n $ip
+  echo "nmap opcion minrate y triple vervose"
+    nmap -p- -sS --min-rate 5000 --open -vvv -n $ip "aqui faltaria lo que viene siendo          en que formato deseas exportarlo"
 }
 
 main $@
