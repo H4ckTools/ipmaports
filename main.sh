e92444145436674ee6f8f83314cc341b76c38a12
Gabriel <gfranklings@gmail.com>
2022-01-26 17:19:50 -0400

    added banner

--- main.sh
+++ main.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+# changing the script pwd
+cd $(dirname $0)
+
+# imports
+
+# ui/banner: provides the function: banner
+source ./ui/banner.sh
+
+# main function
+main () {
+  banner
+}
+
+main
