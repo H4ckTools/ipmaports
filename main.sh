29aac0473431a012197de7a7bb9e286edc11d29a
Gabriel <gfranklings@gmail.com>
2022-01-29 08:47:03 -0400

    fixed bugs

--- main.sh
+++ main.sh
@@ -27,7 +27,7 @@ main () {
     read filename
     printf "${NC}"
   done
- formats='N X S G A'
+  formats='N X S G A'
   printf "${BLUE}${BOLD}> Formato de exportación de evidencia (${formats}): ${GREEN}"
   read format
   ok=0
@@ -39,39 +39,35 @@ main () {
   if [[ $ok == 0 ]]; then
     error "Formato inválido, formatos son ${formats}"
   fi
-printf "${BLUE}${BOLD}> Quieres min-rate? (y/n) ${GREEN}${NC}"
-read want_min_rate
-
-while [[ $want_min_rate != 'y' && $want_min_rate != 'n' ]]; do
-  printf "${RED}${BOLD}> Ingresa y o n ${GREEN}${NC}"
+  printf "${BLUE}${BOLD}> Quieres min-rate? (y/n) ${GREEN}${NC}"
   read want_min_rate
-done
 
-if [[ $want_min_rate == 'y' ]]; then
-  # ejecutas aqui el de min-rate
-else
-
-printf "${BLUE}${BOLD}> Plantilla de temporizado para nmap (1, 2, 3, 4 o 5): ${GREEN}"
-  read temporizing_level
-  printf "${NC}"
-  ok=0
-  for n in {1..5}; do
-    if [[ $temporizing_level == $n ]]; then
-      ok=1
-    fi
+  while [[ $want_min_rate != 'y' && $want_min_rate != 'n' ]]; do
+    printf "${RED}${BOLD}> Ingresa y o n ${GREEN}${NC}"
+    read want_min_rate
   done
-  if [[ $ok == 0 ]]; then
-    error "Plantilla de temporizado debe ser 1, 2, 3, 4 o 5"
-  fi  # ejecutas aqui el normal
-fi
- 
   cmd "ping -c 1 $ip" "Enviando paquetes a la ip"
-  cmd "nmap -p- --open -T${temporizing_level} -v -n $ip" "Escaneando puertos de la ip con nmap"
-  cmd "nmap -p- -sS --min-rate 5000 --open -vvv -n $ip -o${format} ${filename}" "Ejecutando nmap con opcion minrate y exportando archivo $filename"
-  if [[ $format == 'G' ]]; then
-    printf "${BLUE}${BOLD}Intentando extraer puertos con extractPorts (por s4vitar)${NC}\n"
-    extractPorts $filename
-    printf "${GREEN}${BOLD}OK, intenta usar Ctrl-V para pegar los puertos.${NC}\n"
+  if [[ $want_min_rate == 'y' ]]; then
+    cmd "nmap -p- -sS --min-rate 5000 --open -vvv -n $ip -o${format} ${filename}" "Ejecutando nmap con opcion minrate y exportando archivo $filename"
+    if [[ $format == 'G' ]]; then
+      printf "${BLUE}${BOLD}Intentando extraer puertos con extractPorts (por s4vitar)${NC}\n"
+      extractPorts $filename
+      printf "${GREEN}${BOLD}OK, intenta usar Ctrl-V para pegar los puertos.${NC}\n"
+    fi
+  else
+    printf "${BLUE}${BOLD}> Plantilla de temporizado para nmap (1, 2, 3, 4 o 5): ${GREEN}"
+    read temporizing_level
+    printf "${NC}"
+    ok=0
+    for n in {1..5}; do
+      if [[ $temporizing_level == $n ]]; then
+        ok=1
+      fi
+    done
+    if [[ $ok == 0 ]]; then
+      error "Plantilla de temporizado debe ser 1, 2, 3, 4 o 5"
+    fi
+    cmd "nmap -p- --open -T${temporizing_level} -v -n $ip" "Escaneando puertos de la ip con nmap"
   fi
 }
 
