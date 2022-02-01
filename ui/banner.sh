abd878b604d4ce3c7fb22a683f8605c3f5d0073a
Gabriel <gfranklings@gmail.com>
2022-02-01 06:01:52 -0400

    centered warning

--- ui/banner.sh
+++ ui/banner.sh
@@ -9,7 +9,7 @@ banner () {
   clear
   printf "${RED}$(cat $ROOT/plain/banner.txt)${NC}\n"
   printf "${GREEN}${BOLD}\nBy AlphaTechnolog and KOK41${NORMAL}\n\n"
-  printf "${RED}${BOLD}Warning:${NC}${NORMAL}\n"
+  printf "                       ${RED}${BOLD}Warning:${NC}${NORMAL}\n"
   printf "  ${ORANGE}This Tool is made for educational purpose only${RED}${BOLD}!${NC}\n"
   printf "  ${ORANGE}Author will not be responsible for any misuse of this toolkit${RED}${BOLD}!${NC}\n"
   echo
