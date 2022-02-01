a5b8716c7c0038f88fc9a5c9a411a45db241cdb2
Gabriel <gfranklings@gmail.com>
2022-02-01 05:56:46 -0400

    added usage warnings

--- ui/banner.sh
+++ ui/banner.sh
@@ -9,4 +9,8 @@ banner () {
   clear
   printf "${RED}$(cat $ROOT/plain/banner.txt)${NC}\n"
   printf "${GREEN}${BOLD}\nBy AlphaTechnolog and KOK41${NORMAL}\n\n"
+  printf "${RED}${BOLD}Warning:${NC}${NORMAL}\n"
+  printf "${BLUE}This Tool is made for educational purpose only${RED}!${NC}\n"
+  printf "${BLUE}Author will not be responsible for any misuse of this toolkit${RED}!${NC}\n"
+  echo
 }
